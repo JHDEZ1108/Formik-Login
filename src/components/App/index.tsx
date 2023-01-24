76eef15c6c15237f20597a201212cdcf54af4a2c
Josué Hernández <joheca1998@outlook.com>
2023-01-24 11:23:29 -0600

    Adding Capital Form

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Formik, Form } from 'formik';
+
 import "./App.css"
 
+interface FormValues{
+  name: string;
+  position: string;
+}
+
+const initialValues: FormValues = {
+  name: '',
+  position: ''
+}
+
 const App: React.FC = () =>{
+  const handleSubmit = (values: FormValues): void => {
+    alert(JSON.stringify(values));
+  };
+
   return(
     <div className="App">
-      Hello
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+        {props =>{
+          return(
+            <Form>
+              
+            </Form>
+          )
+        }}
+      </Formik>
     </div>
   );
 };
