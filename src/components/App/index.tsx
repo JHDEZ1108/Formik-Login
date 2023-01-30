9744cafee82096c1eb0647f3ee90b05a9e73f772
Josué Hernández <joheca1998@outlook.com>
2023-01-30 17:14:30 -0600

    Agregando validation schema

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Formik, Form } from 'formik';
+import { Formik, Form , Field} from 'formik';
+import * as Yup from 'yup';
 
 import "./App.css"
 
@@ -18,17 +19,50 @@ const App: React.FC = () =>{
     alert(JSON.stringify(values));
   };
 
+  /* Validation schemas */
+  const SignupSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, 'Too short to be a name!')
+      .required('You need to add a name!'),
+    position: Yup.string()
+      .required('You need to add a position')
+  });
+  
   return(
     <div className="App">
       <h1>Sign Up</h1>
       <Formik
         initialValues={initialValues}
         onSubmit={handleSubmit}
+        validationSchema={SignupSchema}
       >
-        {props =>{
+        {({dirty, isValid}) =>{
           return(
             <Form>
+              <div>
+                <label>Name:</label>
+                <Field
+                  placeholder="What's your name?"
+                  name="name"
+                  as="input"
+                />
+              </div>
               
+              <div>
+                <label>Position:</label>
+                <Field
+                  name="position"
+                  as="select"
+                  placeholder="Choose your position"
+                >
+                  <option value="front-end">FrontEnd Developer</option>
+                  <option value="back-end">BackEnd Developer</option>
+                  <option value="devops-engineer">DevOps Engineer</option>
+                  <option value="designer">Designer</option>
+                </Field>
+              </div>
+              
+              <button disabled={!dirty || !isValid} type="submit">Submit</button>
             </Form>
           )
         }}
